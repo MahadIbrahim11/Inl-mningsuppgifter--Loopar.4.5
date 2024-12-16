0d6b963cc57cf70971fb8d364e6807715cb3e45f
maib07001 <maib07001@E-PW05C37V>
2024-12-16 16:17:45 +0100

    Add project files.

---
+++ Inlämningsuppgifter -Loopar.4.5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace inl_4._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("skriv in ett heltal:");
+            string heltal = Console.ReadLine();
+            string nyttTal = "";
+
+            for (int i = 0; i < heltal.Length; i++)
+            {
+                int siffra = int.Parse(heltal[i].ToString());
+                siffra = (siffra + 1) % 10; // öka siffran med 1 och återställ till 0 vid 10
+                nyttTal += siffra.ToString();
+            }
+
+            Console.WriteLine($"det nya talet är: {nyttTal}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
